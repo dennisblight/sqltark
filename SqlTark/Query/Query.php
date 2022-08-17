e180b975238f5e41011abca5be17a4b8dcddbade
Dennis Arfan <dennisarfan@gmail.com>
2022-08-18 05:41:11 +0700

    Update Query stub

--- SqlTark/Query/Query.php
+++ SqlTark/Query/Query.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 
 namespace SqlTark\Query;
 
-class Query
+class Query extends BaseQuery
 {
+    public function newQuery(): BaseQuery
+    {
+        return $this;
+    }
+
+    public function as(string $alias)
+    {
+
+    }
 }
