189df204904d806918785ead72ab16cb8f4d55f5
Dennis Arfan <dennisarfan@gmail.com>
2022-08-21 02:15:25 +0700

    Add test cases

---
+++ Tests/HelperTest.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\TextUI\Help;
+use SqlTark\Helper;
+
+final class HelperTest extends TestCase
+{
+    public function testFlatten_scalar()
+    {
+        $arr = Helper::flatten([1, 2, 3, 4, 5]);
+        $this->assertEquals($arr, [1, 2, 3, 4, 5], 'Scalar array test');
+    }
+
+    public function testFlatten_shallow()
+    {
+        $arr = Helper::flatten([1, [2, 3, 4, 5]]);
+        $this->assertEquals($arr, [1, 2, 3, 4, 5], 'Shallow array ending');
+
+        $arr = Helper::flatten([[1, 2, 3, 4, 5]]);
+        $this->assertEquals($arr, [1, 2, 3, 4, 5], 'Shallow array all');
+
+        $arr = Helper::flatten([[1, 2, 3], 4, 5]);
+        $this->assertEquals($arr, [1, 2, 3, 4, 5], 'Shallow array start');
+
+        $arr = Helper::flatten([[1, 2], [3], [4, 5]]);
+        $this->assertEquals($arr, [1, 2, 3, 4, 5], 'Shallow array each');
+    }
+
+    public function testFlatten_deep()
+    {
+        $arr = Helper::flatten([[[1, 2], [3, [4], 5]]]);
+        $this->assertEquals($arr, [1, 2, 3, 4, 5], 'Deep array test');
+    }
+
+    public function testReplaceAll_itShouldKeepItAsIt()
+    {
+        $callback = function($x) { return $x . ''; };
+
+        $input = '';
+        $output = Helper::replaceAll($input, 'any', $callback);
+        $this->assertEquals($input, $output);
+        
+        $input = null;
+        $output = Helper::replaceAll($input, 'any', $callback);
+        $this->assertEquals($input, $output);
+        
+        $input = ' ';
+        $output = Helper::replaceAll($input, 'any', $callback);
+        $this->assertEquals($input, $output);
+        
+        $input = '  ';
+        $output = Helper::replaceAll($input, 'any', $callback);
+        $this->assertEquals($input, $output);
+        
+        $input = '   ';
+        $output = Helper::replaceAll($input, 'any', $callback);
+        $this->assertEquals($input, $output);
+    }
+
+    public function testReplaceAll_replaceBeginning()
+    {
+        $callback = function() { return '@'; };
+        
+        $output = Helper::replaceAll('hello', '?', $callback);
+        $this->assertEquals('hello', $output);
+        
+        $output = Helper::replaceAll('?hello', '?', $callback);
+        $this->assertEquals('@hello', $output);
+        
+        $output = Helper::replaceAll('??hello', '?', $callback);
+        $this->assertEquals('@@hello', $output);
+        
+        $output = Helper::replaceAll('?? hello', '?', $callback);
+        $this->assertEquals('@@ hello', $output);
+        
+        $output = Helper::replaceAll('? ? hello', '?', $callback);
+        $this->assertEquals('@ @ hello', $output);
+        
+        $output = Helper::replaceAll(' ? ? hello', '?', $callback);
+        $this->assertEquals(' @ @ hello', $output);
+    }
+
+    public function testReplaceAll_replaceEnding()
+    {
+        $callback = function() { return '@'; };
+        
+        $output = Helper::replaceAll('hello', '?', $callback);
+        $this->assertEquals('hello', $output);
+        
+        $output = Helper::replaceAll('hello?', '?', $callback);
+        $this->assertEquals('hello@', $output);
+        
+        $output = Helper::replaceAll('hello??', '?', $callback);
+        $this->assertEquals('hello@@', $output);
+        
+        $output = Helper::replaceAll('hello?? ', '?', $callback);
+        $this->assertEquals('hello@@ ', $output);
+        
+        $output = Helper::replaceAll('hello? ? ', '?', $callback);
+        $this->assertEquals('hello@ @ ', $output);
+        
+        $output = Helper::replaceAll('hello ? ? ', '?', $callback);
+        $this->assertEquals('hello @ @ ', $output);
+    }
+
+    public function testReplaceAll_replaceWithPosition()
+    {
+        $callback = function($x) { return $x; };
+        
+        $output = Helper::replaceAll('hello', '?', $callback);
+        $this->assertEquals('hello', $output);
+        
+        $output = Helper::replaceAll('hello?', '?', $callback);
+        $this->assertEquals('hello0', $output);
+        
+        $output = Helper::replaceAll('hello??', '?', $callback);
+        $this->assertEquals('hello01', $output);
+        
+        $output = Helper::replaceAll('hello?? ', '?', $callback);
+        $this->assertEquals('hello01 ', $output);
+        
+        $output = Helper::replaceAll('hello? ? ', '?', $callback);
+        $this->assertEquals('hello0 1 ', $output);
+        
+        $output = Helper::replaceAll('hello ? ? ', '?', $callback);
+        $this->assertEquals('hello 0 1 ', $output);
+    }
+}
---
+++ Tests/QueryFromTest.php
@@ -0,0 +1,97 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use SqlTark\Component\AdHocTableFromClause;
+use SqlTark\Component\ComponentType;
+use SqlTark\Component\FromClause;
+use SqlTark\Component\QueryFromClause;
+use SqlTark\Query\Query;
+
+class QueryFromTest extends TestCase
+{
+    public function testFrom()
+    {
+        $q = new Query('table');
+
+        $component = $q->getOneComponent(ComponentType::From);
+        $this->assertInstanceOf(FromClause::class, $component);
+    }
+
+    public function testFrom_alias()
+    {
+        $q = new Query('table AS t1');
+
+        /** @var FromClause */
+        $component = $q->getOneComponent(ComponentType::From);
+        $this->assertEquals('t1', $component->getAlias());
+    }
+    
+    public function testFrom_multipleCallShouldReplace()
+    {
+        $q = new Query('table');
+
+        $q->from('table2')->from('table3');
+
+        /** @var FromClause $component */
+        $component = $q->getOneComponent(ComponentType::From);
+        $this->assertInstanceOf(FromClause::class, $component);
+
+        $this->assertEquals('table3', $component->getTable());
+    }
+
+    public function testFromQuery()
+    {
+        $q = new Query;
+        $q2 = new Query('table');
+        $q3 = new Query('table');
+
+        $q->from($q2, 't2');
+        $component = $q->getOneComponent(ComponentType::From);
+        $this->assertInstanceOf(QueryFromClause::class, $component);
+
+        $q->from($q3->alias('t3'));
+        $component = $q->getOneComponent(ComponentType::From);
+        $this->assertInstanceOf(QueryFromClause::class, $component);
+
+        $q->from(function($q) {
+            return $q->from('table')->alias('t4');
+        });
+        $component = $q->getOneComponent(ComponentType::From);
+        $this->assertInstanceOf(QueryFromClause::class, $component);
+    }
+
+    public function testFromAdHoc()
+    {
+        $q = new Query;
+        $q->fromAdHoc('t2', [
+            ['c1' => 'one', 'c1' => 'two', 'c1' => 'three', ]
+        ]);
+
+        /** @var AdHocTableFromClause $component */
+        $component = $q->getOneComponent(ComponentType::From);
+        $this->assertInstanceOf(AdHocTableFromClause::class, $component);
+    }
+
+    public function testFromAdHoc_throwWhenDifferent()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectErrorMessage("Array values count must same with columns count.");
+
+        $q = new Query;
+        $q->fromAdHoc('t2', ['c1', 'c2', 'c3'], [[1, 2, 3], [1, 2, 3, 4], []]);
+
+        /** @var AdHocTableFromClause $component */
+        $component = $q->getOneComponent(ComponentType::From);
+        $this->assertInstanceOf(AdHocTableFromClause::class, $component);
+
+        $q->fromAdHoc('t2', [
+            ['c1' => 'one', 'c1' => 'two', 'c1' => 'three'],
+            [1, 2, 3],
+            [1, 2, 3, 4],
+            [],
+        ]);
+        
+        $this->assertInstanceOf(AdHocTableFromClause::class, $component);
+    }
+}
