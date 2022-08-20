f0259e1ef4cbffb545157b6979e2ce77425ea26e
Dennis Arfan <dennisarfan@gmail.com>
2022-08-21 02:15:03 +0700

    Add interfaces

--- SqlTark/Query/Interfaces/ConditionInterface.php
+++ SqlTark/Query/Interfaces/ConditionInterface.php
@@ -4,31 +4,264 @@ declare(strict_types=1);
 
 namespace SqlTark\Query\Interfaces;
 
-interface ConditionInterface
+interface ConditionInterface extends QueryInterface
 {
-    public function and();
+    /**
+     * Where and condtition
+     * @return static Self object
+     */
+    public function and(): ConditionInterface;
 
-    public function or();
-    
-    public function not(bool $value = true);
+    /**
+     * Where or condtition
+     * @return static Self object
+     */
+    public function or(): ConditionInterface;
 
-    public function where($column, $operator, $value = null);
+    /**
+     * Where not condtition
+     * @param bool $value Is not?
+     * @return static Self object
+     */
+    public function not(bool $value = true): ConditionInterface;
 
-    public function whereIn($column, $values);
+    /**
+     * Where compare two value using ```and``` clause
+     * @return static Self object
+     */
+    public function where($left, $operator = null, $right = null): ConditionInterface;
 
-    public function whereRaw(string $sql, ...$bindings);
+    /**
+     * Where compare two value using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhere($left, $operator = null, $right = null): ConditionInterface;
 
-    public function whereNull($column);
+    /**
+     * Where compare two value using ```not``` clause
+     * @return static Self object
+     */
+    public function whereNot($left, $operator = null, $right = null): ConditionInterface;
 
-    public function whereTrue($column);
+    /**
+     * Where compare two value ```or``` and ```not``` clause
+     * @return static Self object
+     */
+    public function orWhereNot($left, $operator = null, $right = null): ConditionInterface;
 
-    public function whereFalse($column);
+    /**
+     * Where value is in values using ```and``` clause
+     * @return static Self object
+     */
+    public function whereIn($column, $values): ConditionInterface;
 
-    public function whereLike($column, $value, bool $caseSensitive, ?string $escapeCharacter);
+    /**
+     * Where value is in values using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereIn($column, $values): ConditionInterface;
 
-    public function whereBetween($column, $lower, $higher);
+    /**
+     * Where value is in values using ```not``` clause
+     * @return static Self object
+     */
+    public function whereNotIn($column, $values): ConditionInterface;
 
-    public function whereGroup(callable $group);
+    /**
+     * Where value is in values using ```or``` and ```not``` clause
+     * @return static Self object
+     */
+    public function orWhereNotIn($column, $values): ConditionInterface;
 
-    public function whereExists($query);
+    /**
+     * Where raw expression using ```and``` clause
+     * @return static Self object
+     */
+    public function whereRaw(string $expression, ...$bindings): ConditionInterface;
+
+    /**
+     * Where raw expression using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereRaw(string $expression, ...$bindings): ConditionInterface;
+
+    /**
+     * Where value is null using ```and``` clause
+     * @return static Self object
+     */
+    public function whereNull($column): ConditionInterface;
+
+    /**
+     * Where value is null using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereNull($column): ConditionInterface;
+
+    /**
+     * Where value is not null using ```and``` clause
+     * @return static Self object
+     */
+    public function whereNotNull($column): ConditionInterface;
+
+    /**
+     * Where value is not null using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereNotNull($column): ConditionInterface;
+
+    /**
+     * Where value like using ```and``` clause
+     * @return static Self object
+     */
+    public function whereLike($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value like using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereLike($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value like using ```not``` clause
+     * @return static Self object
+     */
+    public function whereNotLike($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value like using ```or``` and ```not``` clause
+     * @return static Self object
+     */
+    public function orWhereNotLike($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value starts with using ```and``` clause
+     * @return static Self object
+     */
+    public function whereStarts($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value starts with using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereStarts($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value starts with using ```not``` clause
+     * @return static Self object
+     */
+    public function whereNotStarts($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value starts with using ```or``` and ```not``` clause
+     * @return static Self object
+     */
+    public function orWhereNotStarts($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value ends with using ```and``` clause
+     * @return static Self object
+     */
+    public function whereEnds($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value ends with using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereEnds($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value ends with using ```not``` clause
+     * @return static Self object
+     */
+    public function whereNotEnds($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value ends with using ```or``` and ```not``` clause
+     * @return static Self object
+     */
+    public function orWhereNotEnds($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value contains using ```and``` clause
+     * @return static Self object
+     */
+    public function whereContains($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value contains using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereContains($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value contains using ```not``` clause
+     * @return static Self object
+     */
+    public function whereNotContains($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value contains using ```or``` and ```not``` clause
+     * @return static Self object
+     */
+    public function orWhereNotContains($column, $value, bool $caseSensitive, ?string $escapeCharacter): ConditionInterface;
+
+    /**
+     * Where value is between two values using ```and``` clause
+     * @return static Self object
+     */
+    public function whereBetween($column, $lower, $higher): ConditionInterface;
+
+    /**
+     * Where value is between two values using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereBetween($column, $lower, $higher): ConditionInterface;
+
+    /**
+     * Where value is between two values using ```not``` clause
+     * @return static Self object
+     */
+    public function whereNotBetween($column, $lower, $higher): ConditionInterface;
+
+    /**
+     * Where value is between two values using ```or``` and ```not``` clause
+     * @return static Self object
+     */
+    public function orWhereNotBetween($column, $lower, $higher): ConditionInterface;
+
+    /**
+     * Perform grouping condition using ```and``` clause
+     * @return static Self object
+     */
+    public function whereGroup(callable $group): ConditionInterface;
+
+    /**
+     * Perform grouping condition using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereGroup(callable $group): ConditionInterface;
+
+    /**
+     * Where values in subquery is exists condition using ```and``` clause
+     * @return static Self object
+     */
+    public function whereExists($query): ConditionInterface;
+
+    /**
+     * Where values in subquery is exists condition using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereExists($query): ConditionInterface;
+
+    /**
+     * Where values in subquery ```not``` exists condition using ```and``` clause
+     * @return static Self object
+     */
+    public function whereNotExists($query): ConditionInterface;
+
+    /**
+     * Where values in subquery is ```not``` exists condition using ```or``` clause
+     * @return static Self object
+     */
+    public function orWhereNotExists($query): ConditionInterface;
 }
---
+++ SqlTark/Query/Interfaces/QueryInterface.php
@@ -0,0 +1,109 @@
+<?php
+declare(strict_types=1);
+namespace SqlTark\Query\Interfaces;
+
+use SqlTark\Query\Query;
+use SqlTark\Query\BaseQuery;
+use SqlTark\Component\AbstractComponent;
+use SqlTark\Component\ComponentType;
+
+interface QueryInterface
+{
+    /**
+     * Get query parent
+     * @return ?BaseQuery Parent
+     */
+    public function getParent(): ?BaseQuery;
+    
+    /**
+     * Set query parent
+     * @param BaseQuery $value Parent
+     */
+    public function setParent(BaseQuery $value): BaseQuery;
+
+    /**
+     * Create new ```Query``` object
+     * @return Query New query
+     */
+    public function newQuery(): Query;
+
+    /**
+     * Create new ```Query``` object then set parent as ```this``` object
+     * @return Query New query
+     */
+    public function newChild(): Query;
+
+    /**
+     * Add new component
+     * 
+     * @param int $componentType Component type from ```ComponentType``` enum class
+     * @param AbstractComponent $component Component object
+     * @param int $engineCode Engine type from ```EngineType``` enum class
+     * @return static Self object
+     */
+    public function addComponent(int $componentType, AbstractComponent $component, int $engineCode = 0): QueryInterface;
+
+    /**
+     * Remove old component with specified ```componentType``` if exists then
+     * add new component.
+     * 
+     * @param int $componentType Component type from ```ComponentType``` enum class
+     * @param AbstractComponent $component Component object
+     * @param int $engineCode Engine type from ```EngineType``` enum class
+     * @return static Self object
+     */
+    public function addOrReplaceComponent(int $componentType, AbstractComponent $component, int $engineCode = 0): QueryInterface;
+
+    /**
+     * Get all components with specified ```componentType``` and ```engineCode```.
+     * Component with no ```engineCode``` (0) also will be added.
+     * 
+     * Get all components when no parameter is specified.
+     * 
+     * @param int $componentType Component type from ```ComponentType``` enum class
+     * @param int $engineCode Engine type from ```EngineType``` enum class
+     * @return AbstractComponent[] Components.
+     */
+    public function getComponents(int $componentType = 0, int $engineCode = 0): array;
+
+    /**
+     * Get single components with specified ```componentType``` and ```engineCode```.
+     * Any component with specified ```componentType``` will be returned if component
+     * with specified ```engineCode``` was not found.
+     * 
+     * @param int $componentType Component type from ```ComponentType``` enum class
+     * @param int $engineCode Engine type from ```EngineType``` enum class
+     * @return ?AbstractComponent Component.
+     */
+    public function getOneComponent(int $componentType, int $engineCode = 0): ?AbstractComponent;
+
+    /**
+     * Check wether components with specified ```componentType``` and ```engineCode``` is exists.
+     * 
+     * @param int $componentType Component type from ```ComponentType``` enum class
+     * @param int $engineCode Engine type from ```EngineType``` enum class
+     * @return bool Is exists.
+     */
+    public function hasComponent(int $componentType, int $engineCode = 0): bool;
+
+    /**
+     * @param int $engine Engine type from ```EngineType``` enum class
+     * @return static Self object
+     */
+    public function for(int $engine, callable $callback): QueryInterface;
+
+    /**
+     * Execute query when condition is fulfilled
+     * 
+     * @param bool $condition Condition
+     * @param callable|null $whenTrue Query when true
+     * @param callable|null $whenFalse Query when false
+     * @return static Self object
+     */
+    public function when(bool $condition, ?callable $whenTrue, ?callable $whenFalse): QueryInterface;
+
+    /**
+     * @return static Self object
+     */
+    public function from($table, ?string $alias = null): QueryInterface;
+}
